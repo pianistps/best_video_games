bd28de59a795711ba9a1f6cfa3fb743d903efe3b
Perla Yates <pianistps@yahoo.com>
2016-08-09 15:01:55 +0000

    tried to fix require

--- lib/best_video_games.rb
+++ lib/best_video_games.rb
@@ -1,3 +1,3 @@
 require_relative "./best_video_games/version"
-require_relative './best_video_games/videogame.rb'
+require_relative './best_video_games/videogame'
 require_relative './best_video_games/cli'
